1654a2ee3688e2601403d303148ee95f2855dc5b
Agus Suroyo <jony.extenz@gmail.com>
2023-12-24 02:17:12 +0700

    a bit code movement

--- src/App.js
+++ src/App.js
@@ -1,166 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import { openai, useAssistant } from './lib/ai';
+import { useAI } from './lib/ai';
 import { useEffect, useRef, useState } from 'react';
 
 function App() {
   const { browserSupportsSpeechRecognition, listening, transcript } = useSpeechRecognition();
 
-  const [assistant, setAssistant] = useState(null);
-  const [thread, setThread] = useState(null);
-  const [messages, setMessages] = useState(null);
-  const [lastMessage, setLastMessage] = useState(null);
-  const [lastMessageText, setLastMessageText] = useState('')
-  const [promt, setPromt] = useState('');
-  const [run, setRun] = useState(null);
-  const [runStatus, setRunStatus] = useState(false);
-  const [checkProgressCount, setCheckProgressCount] = useState(1);
-  const [audioUrl, setAudioUrl] = useState(null);
+  const [command, setCommand] = useState('');
 
   useEffect(() => {
-    if (!listening) {
-      setPromt(transcript);
-    } else {
-      setPromt('');
-    }
-  }, [listening, transcript])
+    setCommand(listening ? '' : transcript);
+  }, [listening, transcript]);
 
-  // 1. prepare assistant
-  useAssistant((assistant) => {
-    setAssistant(assistant);
-  }, []);
 
-  // 2. use thread and create if not exists
-  useEffect(() => {
-    if (assistant) {
-      openai.beta.threads.create().then((th) => setThread(th));
-    }
-  }, [assistant])
+  const { assistant, messageObjectUrl, messageText } = useAI({ command: command, voice: 'echo' });
 
-  // 3. thread and assistant are ready
-  useEffect(() => {
-    if (thread && assistant && !listening && !!promt) {
-      // initially run is null
-      setRun(null);
-      setRunStatus(false);
-      setLastMessageText('');
-
-      // make message
-      openai.beta.threads.messages.create(
-        thread.id,
-        {
-          role: 'user',
-          content: promt,
-        }
-      ).then((r) => {
-        // and run it
-        openai.beta.threads.runs.create(
-          thread.id,
-          {
-            assistant_id: assistant.id,
-          }
-        ).then((r) => {
-
-          // get the run data
-          setRun(r);
-        });
-      });
-    }
-  }, [thread, assistant, listening, promt])
-
-  // 4. get status of run
-  useEffect(() => {
-    let timeout = null
-    if (thread && run) {
-      openai.beta.threads.runs.retrieve(thread.id, run.id)
-        .then((r) => {
-          if (r.status === 'completed') {
-            setRunStatus(true);
-          } else {
-            timeout = setTimeout(() => {
-              setCheckProgressCount(checkProgressCount + 1);
-            }, 1000)
-          }
-        }).catch((e) => {
-          setRunStatus(false);
-        })
-    }
-
-    return () => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-    }
-  }, [thread, run, checkProgressCount])
-
-  // 5. if run status true, then get the list messages
-  useEffect(() => {
-    if (thread && run && runStatus) {
-      openai.beta.threads.messages.list(thread.id).then((r) => {
-        // console.log(r);
-        setMessages(r);
-      })
-    }
-  }, [thread, run, runStatus])
-
-  // 6. set last messages
-  useEffect(() => {
-    if (messages) {
-      setLastMessage(messages?.data[0] || null);
-    }
-  }, [messages])
-
-  // 7. set last message text
-  useEffect(() => {
-    if (lastMessage) {
-      setLastMessageText(lastMessage?.content[0]?.text?.value || '');
-    }
-  }, [lastMessage])
-
-  // 8. turn into audio
-  useEffect(() => {
-    if (lastMessageText) {
-      openai.audio.speech.create({
-        model: "tts-1",
-        voice: "echo",
-        input: lastMessageText
-      }).then((r) => {
-        const reader = r.body.getReader();
-        return new ReadableStream({
-          start(controller) {
-            return pump();
-            function pump() {
-              return reader.read().then(({ done, value }) => {
-                // When no more data needs to be consumed, close the stream
-                if (done) {
-                  controller.close();
-                  return;
-                }
-                // Enqueue the next data chunk into our target stream
-                controller.enqueue(value);
-                return pump();
-              });
-            }
-          },
-        });
-      })
-        // Create a new response out of the stream
-        .then((stream) => new Response(stream))
-        // Create an object URL for the response
-        .then((response) => response.blob())
-        .then((blob) => {
-          const url = URL.createObjectURL(blob);
-          setAudioUrl(url);
-        })
-    }
-  }, [lastMessageText])
-
-  // 9. play audio
+  // play audio
   let audio = useRef();
   useEffect(() => {
-    if (audioUrl) {
-      audio.current = new Audio(audioUrl);
+    if (messageObjectUrl) {
+      audio.current = new Audio(messageObjectUrl);
       audio.current.play();
       console.log('playing');
     }
@@ -171,7 +31,16 @@ function App() {
         audio.current = null;
       }
     }
-  }, [audioUrl])
+  }, [messageObjectUrl])
+
+  // when listening, need to pause audio
+  useEffect(() => {
+    if (listening && audio) {
+      if (audio.current) {
+        audio.current.pause();
+      }
+    }
+  }, [listening]);
 
   if (!assistant) {
     return <span>No Assistant Available</span>;
@@ -189,10 +58,10 @@ function App() {
         {!listening && (
           <button onClick={SpeechRecognition.startListening}>Listen</button>
         )}
-        <label>Promt:</label>
+        <label>Command:</label>
         <p>{transcript}</p>
         <label>Answer:</label>
-        <p>{lastMessageText}</p>
+        <p>{messageText}</p>
       </header>
     </div>
   );
--- src/lib/ai.js
+++ src/lib/ai.js
@@ -6,10 +6,151 @@ export const openai = new OpenAI({
     dangerouslyAllowBrowser: true
 });
 
-export const useAssistant = (callback = (assistant) => {} , listen = []) => {
-    return useEffect(() => {
+export const useAI = ({ command = '', voice = 'echo' }) => {
+    const [state, setState] = useState({
+        assistant: null,
+        thread: null,
+        run: null,
+        runIsComplete: false,
+        countOfRefreshRun: 0,
+        messages: null,
+        message: null,
+        messageText: '',
+        messageBlob: null,
+        messageObjectUrl: null
+    })
+    const { assistant, thread, run, countOfRefreshRun, messages, message, messageText, runIsComplete } = state
+
+    // 1. prepare assistant
+    useEffect(() => {
         openai.beta.assistants.list().then((r) => {
-            callback(r.data[0] || null)
+            setState((s) => {
+                return { ...s, assistant: r.data[0] || null }
+            })
         })
-    }, listen)
+    }, [])
+
+    // 2. use thread and create if not exists
+    useEffect(() => {
+        if (assistant) {
+            openai.beta.threads.create().then((th) => setState((prevState) => ({ ...prevState, thread: th })));
+        }
+    }, [assistant])
+
+    // 3. thread and assistant are ready
+    useEffect(() => {
+        if (thread && assistant && !!command) {
+            // make message
+            openai.beta.threads.messages.create(
+                thread.id,
+                {
+                    role: 'user',
+                    content: command,
+                }
+            ).then((r) => {
+                // initially run is null
+                setState((prevState) => ({ ...prevState, run: null, runIsComplete: false }));
+
+                // and run it
+                openai.beta.threads.runs.create(
+                    thread.id,
+                    {
+                        assistant_id: assistant.id,
+                    }
+                ).then((r) => {
+
+                    // get the run data
+                    setState((prevState) => ({ ...prevState, run: r }));
+                });
+            });
+        }
+    }, [thread, assistant, command])
+
+    // 4. get status of run
+    useEffect(() => {
+        let timeout = null
+        if (thread && run) {
+            openai.beta.threads.runs.retrieve(thread.id, run.id)
+                .then((r) => {
+                    if (r.status === 'completed') {
+                        setState((prevState) => ({ ...prevState, runIsComplete: true }));
+                    } else {
+                        timeout = setTimeout(() => {
+                            setState((prevState) => ({ ...prevState, countOfRefreshRun: countOfRefreshRun + 1 }));
+                        }, 1000)
+                    }
+                }).catch((e) => {
+                    setState((prevState) => ({ ...prevState, runIsComplete: false }));
+                })
+        }
+
+        return () => {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+        }
+    }, [thread, run, countOfRefreshRun])
+
+    // 5. if run status true, then get the list messages
+    useEffect(() => {
+        if (thread && run && runIsComplete) {
+            openai.beta.threads.messages.list(thread.id).then((r) => {
+                setState((prevState) => ({ ...prevState, messages: r }));
+            })
+        }
+    }, [thread, run, runIsComplete])
+
+    // 6. set last messages
+    useEffect(() => {
+        if (messages) {
+            setState((prevState) => ({ ...prevState, message: messages?.data[0] || null }));
+        }
+    }, [messages])
+
+    // 7. set last message text
+    useEffect(() => {
+        if (message) {
+            setState((prevState) => ({ ...prevState, messageText: message?.content[0]?.text?.value || '' }))
+        }
+    }, [message])
+
+    // 8. turn into blob audio
+    useEffect(() => {
+        if (messageText) {
+            openai.audio.speech.create({
+                model: "tts-1",
+                voice: voice,
+                input: messageText
+            }).then((r) => {
+                const reader = r.body.getReader();
+                return new ReadableStream({
+                    start(controller) {
+                        return pump();
+                        function pump() {
+                            return reader.read().then(({ done, value }) => {
+                                // When no more data needs to be consumed, close the stream
+                                if (done) {
+                                    controller.close();
+                                    return;
+                                }
+                                // Enqueue the next data chunk into our target stream
+                                controller.enqueue(value);
+                                return pump();
+                            });
+                        }
+                    },
+                });
+            })
+                // Create a new response out of the stream
+                .then((stream) => new Response(stream))
+                // Create an object URL for the response
+                .then((response) => response.blob())
+                .then((blob) => {
+                    const url = URL.createObjectURL(blob);
+                    setState((prevState) => ({ ...prevState, messageBlob: blob, messageObjectUrl: url }));
+                })
+        }
+    }, [messageText])
+
+    return state
 }
